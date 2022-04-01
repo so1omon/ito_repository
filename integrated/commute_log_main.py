46eddf0698f72ae067c69f6c466f623e732e4210
so1omon <solmin3665@gmail.com>
2022-04-01 16:22:16 +0900

    0401 수정

--- integrated/commute_log_main.py
+++ integrated/commute_log_main.py
@@ -1,57 +1,73 @@
 import cx_Oracle # Oracle DB 연동
 import pymysql # Maria DB 연동 
 import pandas as pd # dataframe 사용을 위한 패키지
-import sys, traceback
-import login_info
+import os, sys, traceback
+import platform
+from login_info import cx_Oracle_info as cx_info, pymysql_info as mysql_info, check_location
+from validate import isDate
+from datetime import timedelta, datetime
+import query
 
-"""
+os_name=platform.system() # 운영체제 정보 (Windows/Linux)
+# os_name='Linux' # 테스트용
 
-인천관광공사 직원 근태 기록 생성기
+interval_sta=interval_end='' # 기록 생성 시작 및 끝기간
+one_day=timedelta(days=1)
 
-2022.04.01 작성 및 설계 시작
+if os_name=='Windows':
+    
+    print('Commute log 생성 기간을 설정해주세요. 특정 날짜의 기록만 생성하려면 동일한 날짜를 입력해주세요.\n')
+    while True:
+        interval_sta=input('commute log 생성 시작 기간을 설정해주세요. (YYYY-MM-DD)\n').strip()
+        interval_end=input('commute log 생성 끝 기간을 설정해주세요. (YYYY-MM-DD)\n').strip()
+        if isDate(interval_sta, interval_end):
+            print(f'{interval_sta}~{interval_end} 기록 생성')
+            interval_sta=datetime.strptime(interval_sta,"%Y-%m-%d")
+            interval_end=datetime.strptime(interval_end,"%Y-%m-%d")
+            break
+elif os_name=='Linux':
+    interval_sta=datetime.now()-one_day
+    interval_end=datetime.now()-one_day
+    print(f"{interval_sta.strftime('%Y-%m-%d')}~{interval_end.strftime('%Y-%m-%d')} 기록 생성")
+else: # Windows나 리눅스가 아닐 때 강제 종료
+    print('Unknown OS version. \nProgram exit')
+    sys.exit() 
+    
+   
+    
+try:
+    # 환경변수 세팅 및 DB 접속
+    os.environ["PATH"]=f'{os.environ["PATH"]};{check_location(os_name)}' #프로그램 실행 중인 동안만 PATH 환경변수에 instant client path 추가
+    
+    print('Try to access Oracle DB...')
+    ora_conn = cx_Oracle.connect(cx_info.get('id'),cx_info.get('pw'),cx_info.get('host')) 
+    ora_cur = ora_conn.cursor() # oracle 접속시 사용할 cursor
+    print('Oracle access successfully!')
+    
+    print('Try to access Maria DB')
+    mysql_conn=pymysql.connect(**mysql_info)
+    mysql_cur=mysql_conn.cursor() # mariadb 접속시 사용할 cursor
+    print('MariDB access successfully!')
+    
+    # 날짜별 루프 돌기
+    while interval_sta<=interval_end:
+        print(f'{interval_sta} 시작')
+        
+        days_offset=(interval_end-interval_sta).days # 오늘기준 날짜랑 얼마나 차이나는지
+        interval_sta=interval_sta+one_day
+        
+        ora_cur.execute(query.oracle_get_appl_sql.format(days_offset)) 
+        print(list(ora_cur))
+        
+    
+except Exception as e:
+    print(e)
+    traceback.print_exc()
+    sys.exit()
 
-cf) [] : DB 또는 dataframe 컬럼명
-
-1. 전체 예외처리를 위한 try문 작성
-
-2. DB connect
- - 1) 특정 날짜 2) 기간 설정
- - 날짜기간 지정하기 위한 input() form 작성
- - 반복문이 돌아가는 날짜를 today(또는 target_day)에 저장
- 
-3. origin_table 생성
- - 초기 신청 정보 가져오기
- - [appl_id] 또는 ymd가 null이면 유효하지 않은 신청 정보로 판단하여 삭제
- - [DEL_YN] == 'Y'(결재취소)인 정보 삭제
- - [appl_id] in [bf_appl_id]이면 삭제
- ? [TYPE] == '1044'인 데이터의 YMD를 현재 날짜로 변경
- - [TIME] <= [STA_HM(hhmm)]~[END_HM(hhmm)]
- - [TIME] == 'NULL~NULL'인 데이터를 '0000~0000'으로 변경
- - dataframe에 삽입 시 데이터베이스에서 null값을 가져오면 'None'으로 초기화
- 
-4. merge_table 생성
- - 기존 컬럼에서 dayoff(4), overtime(2~4), busi_trip(4) 삭제
- - connect.hr_info에서 사번, 이름, 조직 가져와서 merge_table에 target_day 날짜 정보와 같이 데이터 추가
- - 3번과 동일하게 dataframe에 삽입 시 데이터베이스에서 null값을 가져오면 'None'으로 초기화
- - origin_table의 정보를 각 사원별로 merge table에 축적시키기
-   +) [shift_cd], [work_type]은 따로 가져오기
- - 초과근무 내역과 [shift_cd], [work_type] 반영해서 계획시간 만들기
- - connect.at_att_inout에서 출퇴근정보 조회 후 inout에 반영 
-   +) 대휴, 재택을 제외한 09~18 근무자들의 inout은 무조건 default로 0900~1800으로 설정
- - 계획시간과 출퇴근시간 반영해서 확정시간 만들기
- - 확정시간과 [shift_cd], [work_type]을 기준으로 초과근무, 급량비 계산
-   +) 3급 이상 또는 4급 팀장인 경우 (또는 일반직이면서 연봉제인 사원) 확정시간이 아니라 inout을 기준으로 급량비 계산
- 
-5. 중계 DB에 merge_table result push
- - Windows에서는 good.ehr_cal_test로, Linux(중계 DB server) 에서는 connect.ehr_cal로 insert
- 
-Additional Requirements & Feedback
- - 대휴와 주말근무를 정확히 구분하여 로직 반영
- - 육아기/임신기 근로단축 시 초과근무에 대한 기준 명확히 할 것
- 
-9999.12.31 제작 완료
-
-"""
+finally:
+    # DB connection close()
+    pass
 
 
 
--- integrated/login_info.py
+++ integrated/login_info.py
@@ -1,22 +1,18 @@
 import os
-import platform
 
-def check_os(): # 운영체제명 가져오기 (윈도우, 리눅스)
-    os_name=platform.system() 
+def check_location(os_name): # 운영체제에 맞는 client location 가져오기 (윈도우, 리눅스)
     if os_name=='Windows':
         return 'instantclient_21_3'
     elif os_name=='Linux':
         return 'instantclient_21_4'
+# instantclient 환경변수 추가할 때 필요한 디렉토리명 리턴
+# Windows에서는 v21_3, Linux(중계 DB server) 에서는 v21_4 사용 중
 
 cx_Oracle_info={ # eHR Oracle DB 접속 정보
     "id":"silver",
     "pw":"silver",
-    "host":"192.168.20.13:1521/IDTCORA",
-    "LOCATION": check_os() 
-    # instantclient 환경변수 추가할 때 필요한 디렉토리명 리턴
-    # Windows에서는 v21_3, Linux(중계 DB server) 에서는 v21_4 사용 중
+    "host":"192.168.20.13:1521/IDTCORA"
 }
-
 pymysql_info={ # 중계 DB (Maria) 접속 정보
     'host':'192.168.20.19',
     'user':'root',
