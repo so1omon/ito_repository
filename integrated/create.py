99275f96605ed054832acfffbd81a6290eb92e39
sohyun <park.sohyun0430@gmail.com>
2022-04-18 10:30:08 +0900

    220418 reward time 반영

--- integrated/create.py
+++ integrated/create.py
@@ -17,22 +17,22 @@ def make_plan(merge_table):
         # shift_cd = merge_table.loc[i]['SHIFT_CD']
         work_type = merge_table.loc[i]['WORK_TYPE']
         overtime = merge_table.loc[i]['OVER1_TIME']
+        reward_time = merge_table.loc[i]['REWARD_TIME']
         
         data = lib.work_state(work_type)        # {work_time,work_home,work_weekend}
         if overtime != 'None':
-                # 초과근무시간이 있는 경우 OverToPlan 함수 적용
+            # 초과근무시간이 있는 경우 OverToPlan 함수 적용
             planTime = overToPlan(overtime,data)
             
+        elif merge_table.loc[i]['REWARD_TIME']!='None':
+            #휴일근무자인 경우, overtime은 없고 Reward time을 planTime으로 가짐
+            planTime =  reward_time
+                
         else:
-                #초과근무시간이 없는 경우
+            #초과근무시간도 없고 reward time 도 없는 경우, work type에 따라서 결정
             planTime = data["work_time"][0]+'~'+data["work_time"][1]
         
-        print('변경 전 : '+merge_table.loc[i]['PLAN1'])
-        print('plantime : '+planTime)
         merge_table.at[i,'PLAN1'] = planTime      #plan1 설정
-        print('변경 후 : '+merge_table.loc[i]['PLAN1'])
-        print()
-        merge_table.at[i,'PLAN2'] = planTime      #plan2 설정
     print(merge_table['PLAN1'])
     
     
